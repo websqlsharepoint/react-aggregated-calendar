7192936318a9541e34ad8acb6e8fd8231c021c93
Jihong Zhang <zhangj@Ochin.org>
2019-04-02 17:41:52 -0700

    First commit

---
+++ src/webparts/reactAggregatedCalendar/model/SelectedCalendar.ts
@@ -0,0 +1,13 @@
+/**
+ * Interface for SelectedCalendar
+ *
+ * @export
+ * @interface SelectedCalendar
+ */
+export interface SelectedCalendar {
+  CalendarTitle: string;
+  SiteUrl: string;
+  CalendarListTitle: string;
+  CalendarFilter: string;
+  Color: string;
+}
---
+++ src/webparts/reactAggregatedCalendar/service/IAggregatedCalendarService.ts
@@ -0,0 +1,13 @@
+import { FullCalendarEvent } from "../model/FullCalendarEvent";
+
+/**
+ * Interface Service for the AggregatedCalendarService
+ *
+ * @export
+ * @interface IAggregatedCalendarService
+ */
+export interface IAggregatedCalendarService {
+  getEventsForCalendar(calendarRestApi: string, calendarColor: string,
+    startDate: string, endDate: string, categoryValue: string): Promise<FullCalendarEvent[]>
+    
+}
